bbe98e61e473238c4522c6ef44ac504952d2cb01
Edoardo Raino <rainoedoardo@gmail.com>
2023-05-22 14:48:26 +0200

    project save on gitHub

--- src/main/java/nomeAzienda/DigitazonfinalProject/presentation/controllers/RecipesController.java
+++ src/main/java/nomeAzienda/DigitazonfinalProject/presentation/controllers/RecipesController.java
@@ -74,40 +74,11 @@ public class RecipesController {
 
 
 
-    @GetMapping("/{id}/categories") //  categoria di ricette associata alla ricetta con l'ID specificato.
+    @GetMapping("/{id}/categories")
     public CategoryDTO getCategory (@PathVariable long id) {
         Recipes recipes = recipesService.getById(id);
         return  convertToRecipesCategoryToDTO(recipes.getCategory());
 
-        //il metodo getRecipesCategory() gestisce una richiesta GET per ottenere una categoria di ricette specifica,
-        // utilizzando l'ID della ricetta come parametro nella richiesta URL. Viene quindi utilizzato il metodo
-        // convertToRecipesCategoryToDTO()
-        // per convertire l'oggetto RecipesCategory in un oggetto RecipesCategoryDTO prima di restituire la risposta.
 
-
-        //Convertire un oggetto in un'entità o in un DTO serve a gestire in modo più efficace l'informazione all'interno di un'applicazione.
-        //
-        //Le entità rappresentano gli oggetti del dominio, ovvero i dati che devono essere memorizzati nel database e
-        // gestiti dall'applicazione. Le entità sono utilizzate per rappresentare
-        // i dati in modo strutturato e fornire metodi per l'accesso e la gestione dei dati.
-        // Tuttavia, spesso le entità contengono informazioni non necessarie o che non devono essere trasmesse al client.
-        //
-        //I DTO, d'altra parte, sono utilizzati per rappresentare solo le informazioni necessarie per una specifica operazione.
-        // In genere, i DTO contengono solo i dati essenziali, e vengono utilizzati per ridurre il traffico di rete e migliorare
-        // le prestazioni dell'applicazione. I DTO possono essere considerati come una rappresentazione più leggera delle entità,
-        // che vengono utilizzati solo per la comunicazione tra il client e il server.
-        //
-        //Nel codice fornito, i metodi convertToDTO() e convertToRecipesCategoryToDTO() vengono utilizzati per convertire gli oggetti
-        // delle entità Recipes e RecipesCategory in oggetti DTO, rispettivamente RecipesDTO e RecipesCategoryDTO. Questi metodi eliminano
-        // le informazioni non necessarie e forniscono solo i dati essenziali, rendendo più efficiente la comunicazione tra il client e il
-        // server.
-        //
-        //D'altra parte, il metodo convertToEntity() viene utilizzato per convertire un oggetto DTO in un'entità.
-        // Questo è utile quando l'applicazione deve salvare i dati nel database, in quanto è necessario utilizzare oggetti di
-        // entità per l'interazione con il database.
-
-
-        // i service fungono da intermediari tra i controller (che gestiscono le richieste del client) e i repository
-        // (che si occupano della persistenza dei dati nel database).
     }
 }
